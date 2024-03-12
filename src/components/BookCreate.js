9340f7b483e4da7e274eb2c03a4730217e93f4da
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-12 14:02:13 +0800

    Add styling to the create book form

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -15,11 +15,12 @@ function BookCreate({ onCreate }) {
     }
 
     return (
-        <div>
+        <div className="book-create">
+            <h3>Add a Book</h3>
             <form onSubmit={handleSubmit}>
                 <label>Title</label>
-                <input value={title} onChange={handleChange} />
-                <button>Create!</button>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button">Create!</button>
             </form>
         </div>
     );

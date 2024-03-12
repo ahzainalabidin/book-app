aa7fdcf6b0e27b22c1731f18f94266ee3f752236
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-12 13:53:13 +0800

    Add form to create book

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
+
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log('Need to add book with: ', title);
+  };
+
   return (
     <div>
+      <BookCreate onCreate={createBook} />
     </div>
   );
+
 }
 
 export default App;
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,7 +1,28 @@
-function BookCreate() {
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+    }
+
     return (
-        <div>BookCreate</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input value={title} onChange={handleChange} />
+                <button>Create!</button>
+            </form>
+        </div>
     );
+
 }
 
 export default BookCreate;

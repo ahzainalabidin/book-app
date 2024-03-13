98291a0377aed9e626bf47eb25056896fdbaac79
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-13 11:23:27 +0800

    Close edit book form upon submitting

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -13,10 +13,15 @@ function BookShow({ book, onDelete, onEdit }) {
         setShowEdit(!showEdit);
     };
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    };
+
     let content = <h3>{book.title}</h3>;
 
     if (showEdit) {
-        content = <BookEdit book={book} onEdit={onEdit} />;
+        content = <BookEdit book={book} onSubmit={handleSubmit} />;
     }
 
     return (

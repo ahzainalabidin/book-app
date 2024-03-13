7eed9983a11bdad19dbf692449f5922fa8176657
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-13 09:31:58 +0800

    Randomize id number

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    setBooks([...books, { id: 123, title }]);
+    setBooks([...books, { id: Math.floor(Math.random() * 9999), title }]);
   };
 
   return (

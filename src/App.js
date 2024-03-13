5740ca7ee7b6714c1dabdf406602597265ae9acd
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-13 14:06:35 +0800

    Create book using API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
 
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    setBooks([...books, { id: Math.floor(Math.random() * 9999), title }]);
+  const createBook = async (title) => {
+
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    setBooks([...books, response.data]);
+
   };
 
   const deleteBookById = (id) => {

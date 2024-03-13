ad22eba92f554a80b4a596646eed4c7fb58b03f3
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-13 10:26:52 +0800

    Handle change for title input

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 function BookEdit() {
+
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
     return (
         <form className="book-edit">
             <label>Title</label>
-            <input className="input" />
+            <input className="input" value={title} onChange={handleChange} />
             <button className="button is-primary">Save</button>
         </form>
     );
+
 }
 
 export default BookEdit;
